d227d2698d8f7c3cda52c2958821de6a9b3f7f0e
Kevin Zukker <kevin_zukker@icloud.com>
2024-03-04 09:41:24 +0200

    riot fix

---
+++ src/test.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a href="/riot.txt">a</a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
